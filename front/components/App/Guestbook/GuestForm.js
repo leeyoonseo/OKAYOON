32c3d729c885db62b441c78e422e02f333ed2fb5
leeyoonseo <okayoon.lee@gmail.com>
2021-02-02 19:10:26 +0900

    방명록
    - form 스타일 작업 중

--- front/components/App/Guestbook/GuestForm.js
+++ front/components/App/Guestbook/GuestForm.js
@@ -1,18 +1,100 @@
 import React from 'react';
 
-import { Button, Form, Input } from 'antd';
 import styled from 'styled-components';
 
-const FormWrap = styled(Form)`
-    input, 
-    textarea,
-    button {
-        font-size: 12px;
-    }
+import { Button, Form, Input } from 'antd';
+import { PictureOutlined } from '@ant-design/icons';
+
+const FormWrap = styled.form`
+
 `;
 
-const TextArea = styled(Input.TextArea)`
+const Textarea = styled.textarea`
+
 `;
+
+const PasswordInput = styled.input`
+
+`;
+
+const ButtonArea = styled.div``;
+
+const PicktureButton = styled.button``;
+const PicktureIcon = styled(PictureOutlined)``;
+const SubmitButton = styled.button``;
+
+// const FormWrap = styled(Form)`
+//     margin: 0;
+
+//     input, 
+//     textarea,
+//     button {
+//         font-size: 12px;
+//     }
+
+//     input:hover,
+//     textarea:hover {
+
+//     }
+
+//     input:focus,
+//     textarea:focus,
+//     input:active,
+//     textarea:active {
+//         border: none;
+//         box-shadow: none;
+//     }
+// `;
+
+// const Textarea = styled(Input.TextArea)`
+//     margin-bottom: 10px;
+//     height: 100px !important;
+//     border: none;
+//     outline: none;
+// `;
+
+// const PasswordInput = styled(Input)`
+//     width: 30%;
+//     float: left;
+//     border: none;
+//     outline: none;
+// `;
+
+// const ButtonArea = styled.div`
+//     width: 70%;
+//     float: right;
+//     text-align: right;
+
+//     button {
+//         cursor: pointer;
+//     }
+
+//     button + button {
+//         margin-left: 10px;
+//     }
+// `;
+
+// const PicktureButton = styled.button`
+//     padding: 0;
+//     line-height: 1;
+//     vertical-align: middle;
+//     background: none;
+//     outline: none;
+//     border: none;
+// `;
+
+// const PicktureIcon = styled(PictureOutlined)`
+//     font-size: 20px;
+//     color: #555;
+// `;
+
+// const SubmitButton = styled.button`
+//     width: 50px;
+//     color: #fff;
+//     background: #999;
+//     border: 1px solid #999;
+//     outline: none;
+// `;
 
 const GuestForm = () => {
     return (
@@ -21,34 +103,38 @@ const GuestForm = () => {
             encType="multipart/form-data" 
             // onFinish={onSubmit}
         >
-            <TextArea
+
+            <textarea>
+
+            </textarea>
+            <Textarea
                 // value={text}
                 // onChange={onChangeText}
                 maxLength={200}
                 placeholder="오늘 기분은 어떠세요?"
             />
-            <div>
-                <input 
-                    type="text" 
-                    placeholder="비밀번호"
-                    maxLength={10}
-                />
+
+            <PasswordInput 
+                type="text" 
+                placeholder="비밀번호"
+                maxLength={10}
+            />
+            
+            <ButtonArea>
                 <input type="file" name="image" multiple hidden 
                     // ref={imageInput} onChange={onChangeImages} 
                 />
-                <Button 
+                <PicktureButton 
                     // onClick={onClickImageUpload}
                 >
-                    사진
-                </Button>
-                <Button 
-                    type="primary" 
-                    style={{ float: 'right' }} 
-                    htmlType="submit"
+                    <PicktureIcon />
+                </PicktureButton>
+                <SubmitButton 
+                    type="submit" 
                 >
                     등록
-                </Button>
-            </div>
+                </SubmitButton>
+            </ButtonArea>
             {/* <div>
                 {imagePaths.map((v, i) => (
                 <div key={v} style={{ display: 'inline-block' }}>
