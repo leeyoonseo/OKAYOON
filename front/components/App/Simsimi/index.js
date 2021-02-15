3be099fe713ab1feee331a640e6990191f004229
leeyoonseo <okayoon.lee@gmail.com>
2021-02-15 17:58:29 +0900

    채팅
    - 스타일 작업 중

--- front/components/App/Simsimi/index.js
+++ front/components/App/Simsimi/index.js
@@ -1,11 +1,41 @@
 import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-
+import styled from 'styled-components';
 import { Avatar } from 'antd';
 
 import WindowDialog from '../../WindowDialog/index';
-import ChatRoom from './ChatRoom';
+// import ChatRoom from './ChatRoom';
+
+const InitWrap = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+`;
+
+const InitInner = styled.div`
+    display: inline-block;
+`;
+
+const Nickname = styled.div`
+    margin-top: 20px;
+    font-size: 14px;
+    font-weight: 700;
+    text-align: center;
+`;
+
+const AccessButton = styled.button`
+    padding: 5px 10px;
+    margin-top: 20px;
+    width: 100%;
+    color: #666;
+    border: 1px solid #666;
+    outline: none;
+    background: none;
+    cursor: pointer;
+`;
 
 const Simsimi = () => {
     const { me } = useSelector((state) => state.user);
@@ -22,31 +52,29 @@ const Simsimi = () => {
         if (res.state) {}
     }, []);
 
-    const onOpenRoom = useCallback(() => {
-        setStartedChat(true);
-    }, []);
-
-    const onCloseRoom = useCallback(() => {
-        setOpenedDialog(true);
-    }, []);
+    const onOpenRoom = useCallback(() => setStartedChat(true), []);
+    const onCloseRoom = useCallback(() => setOpenedDialog(true), []);
     
     return (
         <>
             {!startedChat && (
-                <div>
-                    <div>
-                        <Avatar src={me.avatar} />    
-                    </div>
-                    <div>{me.nickname}</div> 
+                <InitWrap>
+                    <InitInner>
+                        <Avatar 
+                            size={100}
+                            src={me.avatar} 
+                        />    
+                        <Nickname>
+                            123{me.nickname}
+                        </Nickname> 
 
-                    <div>
-                        <button
+                        <AccessButton
                             onClick={onOpenRoom}
                         >
                             접속하기
-                        </button>
-                    </div>
-                </div>
+                        </AccessButton>
+                    </InitInner>
+                </InitWrap>
             )}
 
             {startedChat && (
@@ -74,7 +102,7 @@ const Simsimi = () => {
             {openedDialog && (
                 <WindowDialog 
                     type="confirm"
-                    text="대화내용이 지워집니다.\n진행하시겠습니까?"
+                    text="대화내용이 지워집니다. <br> 진행하시겠습니까?"
                     callback={onCloseDialog}
                 />
             )}
