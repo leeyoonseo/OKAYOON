530112edc4faddd6426aad8eec045db6c6ee35e4
leeyoonseo <okayoon.lee@gmail.com>
2021-04-01 17:55:20 +0900

    cors 문제, www. 추가

--- back/app.js
+++ back/app.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(hpp());
     app.use(helmet());
     app.use(cors({
-        origin: 'http://okayoon.com',
+        origin: ['http://www.okayoon.com', 'http://okayoon.com'],
         credentials: true,
     }));
 
