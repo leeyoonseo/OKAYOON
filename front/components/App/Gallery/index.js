da5c3760478aefab26febfe6d6b9c59a5446041f
leeyoonseo <okayoon.lee@gmail.com>
2021-04-05 13:44:06 +0900

    이미지 주소 수정

--- front/components/App/Gallery/index.js
+++ front/components/App/Gallery/index.js
@@ -5,90 +5,91 @@ import { AppstoreOutlined, SplitCellsOutlined, ProfileOutlined } from '@ant-desi
 import SlideType from './SlideType';
 import CardType from './CardType';
 import ListType from './ListType';
+import { bucketUrl } from '../../../config/config';
 
 const imageData = [
         { 
-            src: '../../gallery/img00.jpg', 
+            src: `${bucketUrl}/gallery/img00.jpg`, 
             title : '랍스타',
             desc: '내가 돈이 넘쳤다면? 랍스타 씨가 말랐다',
         },
         { 
-            src: '../../gallery/img01.jpg', 
+            src: `${bucketUrl}/gallery/img01.jpg`, 
             title : '공원',
             desc: '공기 좋아보이지만 미세먼지',
         },
         { 
-            src: '../../gallery/img02.jpg', 
+            src: `${bucketUrl}/gallery/img02.jpg`, 
             title : '막걸리',
             desc: '비오는 날은 역시..',
         },
         { 
-            src: '../../gallery/img03.jpg', 
+            src: `${bucketUrl}/gallery/img03.jpg`, 
             title : '전시회',
             desc: '죽기전에 달에 가볼 수 있을까?',
         },
         { 
-            src: '../../gallery/img04.jpg', 
+            src: `${bucketUrl}/gallery/img04.jpg`, 
             title : '달',
             desc: '인스타 갬성때문에 찍었지만...',
         },
         { 
-            src: '../../gallery/img05.jpg', 
+            src: `${bucketUrl}/gallery/img05.jpg`, 
             title : '케이크',
             desc: '동네에 케이크 맛집이 있어 좋다',
         },
         { 
-            src: '../../gallery/img06.jpg', 
+            src: `${bucketUrl}/gallery/img06.jpg`, 
             title : '책상',
             desc: '치운건데..',
         },
         { 
-            src: '../../gallery/img07.jpg', 
+            src: `${bucketUrl}/gallery/img07.jpg`, 
             title : '에피타이저',
             desc: '입맛 돋구지 않아도 난 잘 묵는다',
         },
         { 
-            src: '../../gallery/img08.jpg', 
+            src: `${bucketUrl}/gallery/img08.jpg`, 
             title : '식탁',
             desc: '인스타 갬성샷 (각도가 맞나?)',
         },
         { 
-            src: '../../gallery/img09.jpg', 
+            src: `${bucketUrl}/gallery/img09.jpg`, 
             title : '화장실 벽',
             desc: '인스타 갬성샷이지만 여긴 화장실',
         },
         { 
-            src: '../../gallery/img10.jpg', 
+            src: `${bucketUrl}/gallery/img10.jpg`, 
             title : '무지개 떡',
             desc: '빨주노초파남보 내가 다먹음',
         },
         { 
-            src: '../../gallery/img11.jpg', 
+            src: `${bucketUrl}/gallery/img11.jpg`, 
             title : '빵 진열',
             desc: '빵맛 짱맛 국룰',
         },
         { 
-            src: '../../gallery/img12.jpg', 
+            src: `${bucketUrl}/gallery/img12.jpg`, 
             title : '빵 & 커피',
             desc: '빵 맛집이라는데? 뷰 맛집',
         },
         { 
-            src: '../../gallery/img13.jpg', 
+            src: `${bucketUrl}/gallery/img13.jpg`, 
             title : '벽에 꽃',
             desc: '꽃인가 디퓨저인가',
         },
         { 
-            src: '../../gallery/img14.jpg', 
+            src: `${bucketUrl}/gallery/img14.jpg`, 
             title : '정신없는 식사',
             desc: '정신없이 먹었기때문에',
         },
         { 
-            src: '../../gallery/img15.jpg', 
+            src: `${bucketUrl}/gallery/img15.jpg`, 
             title : '하늘',
             desc: '맑은 하늘, 퇴근각!',
         },
         { 
-            src: '../../gallery/img16.jpg', 
+            src: `${bucketUrl}/gallery/img16.jpg`, 
             title : '해산물',
             desc: '난 킬러다. 해산물 킬러',
         },
