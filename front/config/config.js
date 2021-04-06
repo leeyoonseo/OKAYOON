8472f878a775c1e31fabf48b12f0a71dd130f99a
leeyoonseo <okayoon.lee@gmail.com>
2021-04-06 14:02:41 +0900

    https로 요청하도록 backurl 수정

--- front/config/config.js
+++ front/config/config.js
@@ -1,2 +1,2 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.okayoon.com' : 'http://localhost:80';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'https://api.okayoon.com' : 'http://localhost:80';
 export const bucketUrl = 'https://okayoon-bucket.s3.ap-northeast-2.amazonaws.com';
