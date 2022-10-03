24f482781c47bf08ac9a72861a711acad60b4dc4
leeyoonseo <okayoon.lee@gmail.com>
2022-10-03 22:00:04 +0900

    라우트 등록

--- front-re/src/routes/index.tsx
+++ front-re/src/routes/index.tsx
@@ -3,10 +3,35 @@ import loadable from '@loadable/component';
 
 // Code Split
 const Home = loadable(() => import('@pages/Home'));
+const Game = loadable(() => import('@pages/Game'));
+const Guestbook = loadable(() => import('@pages/Guestbook'));
+const Photos = loadable(() => import('@pages/Photos'));
+const Portfolio = loadable(() => import('@pages/Portfolio'));
+const Sleep = loadable(() => import('@pages/Sleep'));
 
 export default createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  }
+  },
+  {
+    path: '/game',
+    element: <Game />,
+  },
+  {
+    path: '/guestbook',
+    element: <Guestbook />,
+  },
+  {
+    path: '/photos',
+    element: <Photos />,
+  },
+  {
+    path: '/portfolio',
+    element: <Portfolio />,
+  },
+  {
+    path: '/sleep',
+    element: <Sleep />,
+  },
 ]);
